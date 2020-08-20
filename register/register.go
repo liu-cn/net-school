c7160da318d3c3fd871c90e487c90fbcc30a12ce
liu-cn <1210227080@qq.com>
2020-08-20 19:52:08 +0800

    注册登录功能收工了

--- register/register.go
+++ register/register.go
@@ -14,14 +14,34 @@ type User struct {
 	Phone string `db:"phone" json:"phone" binding:"required"`
 }
 
-//数据库查询用户是否注册
-func RegisterUser(user *User) bool{
-	fmt.Println("register user:",user)
-	sqlstr:=`insert into userlist(username,password,phone)values(?,?,?)`
-	res:=db.Db.MustExec(sqlstr,user.Username,user.Password,user.Phone)
-	fmt.Println(res)
+type HasUser struct {
+	Username string `db:"username" json:"username" binding:"required"`
+	Phone string `db:"phone" json:"phone" binding:"required"`
+}
 
-	return true
+//数据库验证用户是否已经被注册
+func RegisterUser(user *User) (bool,string){
+	fmt.Println("register user:",user)
+	var hasUser HasUser
+
+	queryStr:=`select username,phone from userlist where username=? or phone=?`
+	if err := db.Db.Get(&hasUser, queryStr, user.Username, user.Phone);err != nil {
+		fmt.Println("Select err:",err,hasUser)
+		sqlstr:=`insert into userlist(username,password,phone)values(?,?,?)`
+		res:=db.Db.MustExec(sqlstr,user.Username,user.Password,user.Phone)
+		fmt.Println(res)
+
+		return true,"注册成功"
+	}else {
+		fmt.Println(hasUser)
+		if hasUser.Username==user.Username{
+			return false,"账号已经被注册！"
+		}else if hasUser.Phone==user.Phone {
+			return false,"手机号已经被注册！"
+		}else {
+			return false,"服务器故障，注册失败！"
+		}
+	}
 }
 
 //前端请求注册时返回的处理函数
@@ -37,7 +57,7 @@ func Register()func(*gin.Context){
 			return
 		}
 		fmt.Println("处理函数的user:",user)
-		registerStatus := RegisterUser(&user)
+		registerStatus,msg := RegisterUser(&user)
 		if registerStatus{
 			token,err:=token.GenerateToken(user.Username,user.Password)
 			if err != nil {
@@ -48,8 +68,15 @@ func Register()func(*gin.Context){
 			}
 			c.JSON(http.StatusOK,gin.H{
 				"registerStatus":"ok",
+				"msg":msg,
 				"token":token,
 				"username":user.Username,
+				"phone":user.Phone,
+			})
+		}else {
+			c.JSON(http.StatusOK,gin.H{
+				"registerStatus":"no",
+				"msg":msg,
 			})
 		}
 
--- views/login/login.go
+++ views/login/login.go
@@ -2,6 +2,7 @@ package login
 
 import (
 	"fmt"
+	"gin/db"
 	"gin/token"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -11,6 +12,18 @@ type UserLogin struct {
 	//结构体这里字段名必须大写，不认接收不到数据。
 	Username string `form:"username" json:"username" uri:"username" binding:"required"`
 	Password string `form:"password" json:"password" uri:"password" binding:"required"`
+}
+
+//验证账号是否存在
+func VerifyAccount(username,password string) (bool,string) {
+	var loginUser UserLogin
+	sqlStr:=`select username,password from userlist where username=? && password=?`
+	if err := db.Db.Get(&loginUser, sqlStr,username,password);err!=nil{
+		fmt.Println("查询结果：err：",err)
+		return false,"账号或密码错误！"
+	}else {
+		return true,"登录成功！"
+	}
 }
 
 
@@ -25,27 +38,28 @@ func Login()func(c *gin.Context){
 			return
 		}
 		fmt.Println(login)
-		if login.Username!="admin" || login.Password!="123456" {
+
+		ok,msg:=VerifyAccount(login.Username,login.Password)
+		if !ok{
 			c.JSON(http.StatusOK,gin.H{
 				"LoginStatus":"failed",
-				"msg":"账号或密码错误！",
+				"msg":msg,
 			})
 			return
-		}
-
-		token,err:=token.GenerateToken(login.Username,login.Password)
-		if err != nil {
-			c.JSON(http.StatusBadGateway,gin.H{
-				"msg":"生成token失败",
+		}else{
+			token,err:=token.GenerateToken(login.Username,login.Password)
+			if err != nil {
+				c.JSON(http.StatusBadGateway,gin.H{
+					"msg":"生成token失败",
+				})
+				return
+			}
+			c.JSON(http.StatusOK,gin.H{
+				"username":login.Username,
+				"password":login.Password,
+				"token":token,
+				"msg": msg,
 			})
-			return
 		}
-		c.JSON(http.StatusOK,gin.H{
-			"username":login.Username,
-			"password":login.Password,
-			"token":token,
-		})
-
-
 	}
 }
